e1fb0e0f16e2a1b91c7834bfe722324f1ae79d2a
gumeeee <gaxco123@gmail.com>
2024-02-27 09:59:47 -0300

    Adicionando input com tratativa de erro já

--- src/locawebfilmes/Menu.java
+++ src/locawebfilmes/Menu.java
@@ -1,16 +1,19 @@
 package locawebfilmes;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import locawebfilmes.util.Cores;
 
 public class Menu {
 	public static void main(String[] args) {
-				
+
 		Scanner sc = new Scanner(System.in);
-		
+
+		int opcao = 0;
+
 		System.out.println(Cores.TEXT_BLACK + Cores.ANSI_PURPLE_BACKGROUND
-						 + "*****************************************************");		
+				+ "*****************************************************");
 		System.out.println("                                                     ");
 		System.out.println("             LOCAWEB-FILMES DO GUIZINHO              ");
 		System.out.println("                                                     ");
@@ -28,9 +31,14 @@ public class Menu {
 		System.out.println("*****************************************************");
 		System.out.println("Entre com a opção desejada:                          ");
 		System.out.println("                                                     " + Cores.TEXT_RESET);
-		
-		
-		
+
+		try {
+			opcao = sc.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("\nDigite valores inteiros!");
+			sc.nextLine();
+			opcao = 0;
+		}
 
 	}
 }
